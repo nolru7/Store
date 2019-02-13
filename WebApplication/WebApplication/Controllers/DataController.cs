338c4ad810693cc4ca336fef2d3e28e2f1850edb
석훈 이 <nolru7@naver.com>
2019-02-13 17:54:02 +0900

    'web'

---
+++ WebApplication/WebApplication/Controllers/DataController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication.Controllers
+{
+    
+    public class DataController : Controller
+    {
+        // GET: api/<controller>
+        [Route("api/Data")]
+        [HttpGet]
+        [EnableCors("AllowSpecificOrigin")]
+        public ArrayList Data()
+        {
+            /*
+             var data = [
+           {"url":"http://google.com","text":"구글"},
+           {"url":"http://naver.com","text":"네이버"},
+           {"url":"http://daum.net","text":"다음"},
+           {"url":"http://gdu.co.kr","text":"구디"},
+           ];
+
+
+             */
+            ArrayList data = new ArrayList();
+            Hashtable ht = new Hashtable();
+            ht.Add("url", "http://google.com");
+            ht.Add("text", "구글");
+            data.Add(ht);
+
+            ht = new Hashtable();
+            ht.Add("url", "http://naver.com");
+            ht.Add("text", "네이버");
+            data.Add(ht);
+
+            ht = new Hashtable();
+            ht.Add("url", "http://daum.net");
+            ht.Add("text", "다음");
+            data.Add(ht);
+
+            ht = new Hashtable();
+            ht.Add("url", "http://gdu.co.kr");
+            ht.Add("text", "구디");
+            data.Add(ht);
+
+
+
+            return data;
+        }
+
+        
+    }
+}
